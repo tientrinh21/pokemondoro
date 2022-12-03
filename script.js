ff6089e1db069947791baa2e6039218cd313d625
tientrinh21 <tqtien.a11619@gmail.com>
2022-12-04 01:52:07 +0900

    Set up boundaries

--- script.js
+++ script.js
@@ -1,38 +1,12 @@
 const canvas = document.querySelector('#main-canvas')
 const c = canvas.getContext('2d')
-
 canvas.width = 960
 canvas.height = 640
 
-function OnResizeCalled() {}
-
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
-
-const image = new Image()
-image.src = './assets/Map.png'
-
-const playerImage = new Image()
-playerImage.src = './assets/boy-down.png'
-
-class Sprite {
-	constructor({ position, velocity, image }) {
-		this.position = position
-		this.image = image
-	}
-
-	draw() {
-		c.drawImage(this.image, this.position.x, this.position.y)
-	}
+const offset = {
+	x: -832,
+	y: -288,
 }
-
-const background = new Sprite({
-	position: {
-		x: -832,
-		y: -288,
-	},
-	image: image,
-})
 
 const keys = {
 	ArrowUp: {
@@ -49,10 +23,74 @@ const keys = {
 	},
 }
 
+const image = new Image()
+image.src = './assets/Map.png'
+
+const playerImage = new Image()
+playerImage.src = './assets/boy-down.png'
+
+const collisionsMap = []
+for (let i = 0; i < collisions.length; i += 30) {
+	collisionsMap.push(collisions.slice(i, 30 + i))
+}
+
+class Boundary {
+	static width = 64
+	static height = 64
+
+	constructor({ position }) {
+		this.position = position
+		this.width = 64
+		this.height = 64
+	}
+
+	draw() {
+		c.fillStyle = 'red'
+		c.fillRect(this.position.x, this.position.y, this.width, this.height)
+	}
+}
+
+const boundaries = []
+collisionsMap.forEach((row, i) => {
+	row.forEach((symbol, j) => {
+		if (symbol)
+			boundaries.push(
+				new Boundary({ position: { x: j * Boundary.width + offset.x, y: i * Boundary.height + offset.y } })
+			)
+	})
+})
+
+class Sprite {
+	constructor({ position, velocity, image }) {
+		this.position = position
+		this.image = image
+	}
+
+	draw() {
+		c.drawImage(this.image, this.position.x, this.position.y)
+	}
+}
+
+const background = new Sprite({
+	position: {
+		x: offset.x,
+		y: offset.y,
+	},
+	image: image,
+})
+
+const testBoundary = new Boundary({ position: { x: 400, y: 400 } })
+
+const movables = [background, testBoundary]
+
 function animate() {
 	window.requestAnimationFrame(animate)
 
 	background.draw()
+	testBoundary.draw()
+	/* boundaries.forEach((boundary) => {
+		boundary.draw()
+	}) */
 
 	c.drawImage(
 		playerImage,
@@ -66,13 +104,27 @@ function animate() {
 		playerImage.height * 2
 	)
 
-	if (keys.ArrowUp.pressed && lastKey === 'ArrowUp') background.position.y += 4
-	else if (keys.ArrowLeft.pressed && lastKey === 'ArrowLeft') background.position.x += 4
-	else if (keys.ArrowDown.pressed && lastKey === 'ArrowDown') background.position.y -= 4
-	else if (keys.ArrowRight.pressed && lastKey === 'ArrowRight') background.position.x -= 4
+	if (keys.ArrowUp.pressed && lastKey === 'ArrowUp') {
+		movables.forEach((moveable) => {
+			moveable.position.y += 4
+		})
+	} else if (keys.ArrowLeft.pressed && lastKey === 'ArrowLeft') {
+		movables.forEach((moveable) => {
+			moveable.position.x += 4
+		})
+	} else if (keys.ArrowDown.pressed && lastKey === 'ArrowDown') {
+		movables.forEach((moveable) => {
+			moveable.position.y -= 4
+		})
+	} else if (keys.ArrowRight.pressed && lastKey === 'ArrowRight') {
+		movables.forEach((moveable) => {
+			moveable.position.x -= 4
+		})
+	}
 }
 animate()
 
+let lastKey = ''
 window.addEventListener('keydown', (e) => {
 	switch (e.key) {
 		case 'ArrowUp':
@@ -94,7 +146,6 @@ window.addEventListener('keydown', (e) => {
 	}
 })
 
-let lastKey = ''
 window.addEventListener('keyup', (e) => {
 	switch (e.key) {
 		case 'ArrowUp':
