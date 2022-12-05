e3819f11a163a29c13135c8f7e7cda878f4ee302
tientrinh21 <tqtien.a11619@gmail.com>
2022-12-05 16:32:41 +0900

    Fix bad crop of character sprite

---
+++ classes.js
@@ -0,0 +1,63 @@
+const tileSize = 64
+
+class Sprite {
+	constructor({ position, velocity, image, frames = { max: 1 }, scale = 1, isCharacter = false, sprites }) {
+		this.position = position
+		this.image = image
+		this.frames = { ...frames, val: 0, elapsed: 0 }
+		this.scale = scale
+		if (isCharacter) this.hitbox = { x: 0, y: 0 }
+		this.image.onload = () => {
+			this.width = (this.image.width / this.frames.max) * this.scale
+			this.height = (this.image.height / 2) * scale
+			if (isCharacter) {
+				this.hitbox.x = this.position.x
+				this.hitbox.y = this.position.y + characterSize.height
+			}
+		}
+		this.moving = false
+		this.sprites = sprites
+	}
+
+	draw() {
+		c.drawImage(
+			this.image,
+			(this.frames.val * this.width) / this.scale + 1, // avoid ugly crop
+			0,
+			this.image.width / this.frames.max - 2, // avoid ugly crop
+			this.image.height,
+			this.position.x,
+			this.position.y,
+			(this.image.width / this.frames.max) * this.scale,
+			this.image.height * this.scale
+		)
+
+		if (!this.moving) {
+			this.frames.val = 0
+			return
+		}
+
+		if (this.frames.max > 1) this.frames.elapsed++
+
+		if (this.frames.elapsed % 10 === 0) {
+			if (this.frames.val < this.frames.max - 1) this.frames.val++
+			else this.frames.val = 0
+		}
+	}
+}
+
+class Boundary {
+	static width = tileSize
+	static height = tileSize
+
+	constructor({ position }) {
+		this.position = position
+		this.width = tileSize
+		this.height = tileSize
+	}
+
+	draw() {
+		c.fillStyle = 'rgba(255, 0, 0, 0.0)'
+		c.fillRect(this.position.x, this.position.y, this.width, this.height)
+	}
+}
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Variables
 const canvas = document.querySelector('#main-canvas')
 const c = canvas.getContext('2d')
 canvas.width = 960
@@ -8,10 +9,7 @@ const offset = {
 	y: -642,
 }
 
-const tileSize = 64
-
-const walkingPace = 4
-let movingPace = walkingPace
+let movingPace = 4
 
 const keys = {
 	ArrowUp: {
@@ -39,86 +37,36 @@ backgroundImage.src = './assets/Map.png'
 const foregroundImage = new Image()
 foregroundImage.src = './assets/Foreground.png'
 
-const playerImage = new Image()
-playerImage.src = './assets/boy-down.png'
+const playerUpImage = new Image()
+playerUpImage.src = './assets/boy-up.png'
 
-const collisionsMap = []
-for (let i = 0; i < collisions.length; i += 42) {
-	collisionsMap.push(collisions.slice(i, 42 + i))
-}
+const playerLeftImage = new Image()
+playerLeftImage.src = './assets/boy-left.png'
 
-class Boundary {
-	static width = tileSize
-	static height = tileSize
+const playerDownImage = new Image()
+playerDownImage.src = './assets/boy-down.png'
 
-	constructor({ position }) {
-		this.position = position
-		this.width = tileSize
-		this.height = tileSize
-	}
+const playerRightImage = new Image()
+playerRightImage.src = './assets/boy-right.png'
 
-	draw() {
-		c.fillStyle = 'rgba(255, 0, 0, 0.0)'
-		c.fillRect(this.position.x, this.position.y, this.width, this.height)
-	}
-}
-
-const boundaries = []
-collisionsMap.forEach((row, i) => {
-	row.forEach((symbol, j) => {
-		if (symbol)
-			boundaries.push(
-				new Boundary({ position: { x: j * Boundary.width + offset.x, y: i * Boundary.height + offset.y } })
-			)
-	})
-})
-
-class Sprite {
-	constructor({ position, velocity, image, frames = { max: 1 }, scale = 1, isCharacter = false }) {
-		this.position = position
-		this.image = image
-		this.frames = frames
-		this.scale = scale
-		if (isCharacter) this.hitbox = { x: 0, y: 0 }
-		this.image.onload = () => {
-			this.width = (this.image.width / this.frames.max) * this.scale
-			this.height = (this.image.height / 2) * scale
-			if (isCharacter) {
-				this.hitbox.x = this.position.x
-				this.hitbox.y = this.position.y + characterSize.height
-			}
-			/* console.log(this.width)
-			console.log(this.height)
-			console.log(this.hitbox) */
-		}
-	}
-
-	draw() {
-		c.drawImage(
-			this.image,
-			0,
-			0,
-			this.image.width / this.frames.max,
-			this.image.height,
-			this.position.x,
-			this.position.y,
-			(this.image.width / this.frames.max) * this.scale,
-			this.image.height * this.scale
-		)
-	}
-}
-
+// Objects
 const player = new Sprite({
 	position: {
 		x: canvas.width / 2 - characterSize.width,
 		y: canvas.height / 2 - characterSize.height,
 	},
-	image: playerImage,
+	image: playerDownImage,
 	frames: {
 		max: 4,
 	},
 	scale: 2,
 	isCharacter: true,
+	sprites: {
+		up: playerUpImage,
+		left: playerLeftImage,
+		down: playerDownImage,
+		right: playerRightImage,
+	},
 })
 
 const background = new Sprite({
@@ -134,18 +82,31 @@ const foreground = new Sprite({
 		x: offset.x,
 		y: offset.y,
 	},
-	image: backgroundImage,
+	image: foregroundImage,
 })
 
-const testBoundary = new Boundary({ position: { x: 320, y: 320 } })
-// const testBoundary = new Boundary({ position: { x: 448, y: 320 + 64 } })
+const collisionsMap = []
+for (let i = 0; i < collisions.length; i += 42) {
+	collisionsMap.push(collisions.slice(i, 42 + i))
+}
 
-const movables = [background, ...boundaries, testBoundary]
+const boundaries = []
+collisionsMap.forEach((row, i) => {
+	row.forEach((symbol, j) => {
+		if (symbol)
+			boundaries.push(
+				new Boundary({ position: { x: j * Boundary.width + offset.x, y: i * Boundary.height + offset.y } })
+			)
+	})
+})
+
+const movables = [background, ...boundaries, foreground]
 
 function objectCollision({ player, object }) {
 	return (
-		player.hitbox.x + player.width > object.position.x && // left of wall
-		player.hitbox.x < object.position.x + object.width && // right of  wall
+		// add/minus addition movingPace to avoid border weird behavior
+		player.hitbox.x + player.width > object.position.x + movingPace && // left of wall
+		player.hitbox.x < object.position.x + object.width - movingPace && // right of  wall
 		player.hitbox.y + player.height > object.position.y + movingPace && // bottom of wall
 		player.hitbox.y < object.position.y + object.height - movingPace // top off wall
 	)
@@ -162,6 +123,7 @@ function animate() {
 		boundary.draw()
 	})
 	player.draw()
+	foreground.draw()
 
 	movables.forEach((moveable) => {
 		if (moving) {
@@ -172,9 +134,13 @@ function animate() {
 
 	futureStep.x -= Math.sign(futureStep.x) * movingPace
 	futureStep.y -= Math.sign(futureStep.y) * movingPace
+	if (!futureStep.x && !futureStep.y) {
+		player.moving = false
+	}
 
 	if (keys.ArrowUp.pressed && lastKey === 'ArrowUp') {
-		playerImage.src = './assets/boy-up.png'
+		player.moving = true
+		player.image = player.sprites.up
 
 		if (!futureStep.x && !futureStep.y) {
 			futureStep.y = tileSize
@@ -189,14 +155,13 @@ function animate() {
 					object: { ...boundary, position: { x: boundary.position.x, y: boundary.position.y + movingPace * 2 } },
 				})
 			) {
-				console.log('Colliding')
 				moving = false
-				// futureStep = { x: 0, y: 0 }
 				break
 			}
 		}
 	} else if (keys.ArrowLeft.pressed && lastKey === 'ArrowLeft') {
-		playerImage.src = './assets/boy-left.png'
+		player.moving = true
+		player.image = player.sprites.left
 
 		if (!futureStep.x && !futureStep.y) {
 			futureStep.x = tileSize
@@ -208,16 +173,16 @@ function animate() {
 			if (
 				objectCollision({
 					player: player,
-					object: { ...boundary, position: { x: boundary.position.x + movingPace, y: boundary.position.y } },
-				})
+					object: { ...boundary, position: { x: boundary.position.x + movingPace * 2, y: boundary.position.y } },
+				}) // predicting next move, movingPace * 2 to make sure player don't move on half way
 			) {
-				console.log('Colliding')
 				moving = false
 				break
 			}
 		}
 	} else if (keys.ArrowDown.pressed && lastKey === 'ArrowDown') {
-		playerImage.src = './assets/boy-down.png'
+		player.moving = true
+		player.image = player.sprites.down
 
 		if (!futureStep.x && !futureStep.y) {
 			futureStep.y = -tileSize
@@ -229,16 +194,16 @@ function animate() {
 			if (
 				objectCollision({
 					player: player,
-					object: { ...boundary, position: { x: boundary.position.x, y: boundary.position.y - movingPace } },
+					object: { ...boundary, position: { x: boundary.position.x, y: boundary.position.y - movingPace * 2 } },
 				})
 			) {
-				console.log('Colliding')
 				moving = false
 				break
 			}
 		}
 	} else if (keys.ArrowRight.pressed && lastKey === 'ArrowRight') {
-		playerImage.src = './assets/boy-right.png'
+		player.moving = true
+		player.image = player.sprites.right
 
 		if (!futureStep.x && !futureStep.y) {
 			futureStep.x = -tileSize
@@ -250,10 +215,9 @@ function animate() {
 			if (
 				objectCollision({
 					player: player,
-					object: { ...boundary, position: { x: boundary.position.x - movingPace, y: boundary.position.y } },
+					object: { ...boundary, position: { x: boundary.position.x - movingPace * 2, y: boundary.position.y } },
 				})
 			) {
-				console.log('Colliding')
 				moving = false
 				break
 			}
@@ -267,19 +231,22 @@ window.addEventListener('keydown', (e) => {
 	switch (e.key) {
 		case 'ArrowUp':
 			keys.ArrowUp.pressed = true
-			if (!keys[lastKey].pressed) lastKey = 'ArrowUp'
+			if (!keys[lastKey].pressed && !futureStep.x && !futureStep.y) lastKey = 'ArrowUp'
 			break
 		case 'ArrowLeft':
 			keys.ArrowLeft.pressed = true
-			if (!keys[lastKey].pressed) lastKey = 'ArrowLeft'
+			if (!keys[lastKey].pressed && !futureStep.x && !futureStep.y) lastKey = 'ArrowLeft'
 			break
 		case 'ArrowDown':
 			keys.ArrowDown.pressed = true
-			if (!keys[lastKey].pressed) lastKey = 'ArrowDown'
+			if (!keys[lastKey].pressed && !futureStep.x && !futureStep.y) lastKey = 'ArrowDown'
 			break
 		case 'ArrowRight':
 			keys.ArrowRight.pressed = true
-			if (!keys[lastKey].pressed) lastKey = 'ArrowRight'
+			if (!keys[lastKey].pressed && !futureStep.x && !futureStep.y) lastKey = 'ArrowRight'
+			break
+		case 'z':
+			movingPace = runningPace
 			break
 	}
 })
