03bfdc09a44c4c010fc88890d6e3a197b574a7a2
tientrinh21 <tqtien.a11619@gmail.com>
2022-12-11 09:37:09 +0900

    Add auth

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log(firebase)
+const auth = firebase.auth()
+
+const whenSignedIn = document.querySelector('#whenSignedIn')
+const whenSignedOut = document.querySelector('#whenSignedOut')
+
+const signInBtn = document.querySelector('#signInBtn')
+const signOutBtn = document.querySelector('#signOutBtn')
+
+const userDetails = document.querySelector('#userDetails')
+
+const provider = new firebase.auth.GoogleAuthProvider()
+
+/// Sign in event handlers
+signInBtn.onclick = () => auth.signInWithPopup(provider)
+
+signOutBtn.onclick = () => auth.signOut()
+
+auth.onAuthStateChanged((user) => {
+	if (user) {
+		// signed in
+		whenSignedIn.hidden = false
+		whenSignedOut.hidden = true
+		// userDetails.innerHTML = `Hello ${user.displayName}! User ID: ${user.uid}`
+		userDetails.innerHTML = `${user.displayName}`
+	} else {
+		// not signed in
+		whenSignedIn.hidden = true
+		whenSignedOut.hidden = false
+		userDetails.innerHTML = ``
+	}
+})
